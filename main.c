407c24d8de285731e9ecf6709c74f0b7c5dcdcf7
tanaxer01 <jpbrione@gmail.com>
2021-09-30 23:25:43 -0300

    rows R shifting

--- main.c
+++ main.c
@@ -3,32 +3,54 @@
 #include <stdio.h>
 
 // Matrix ops
-char* RotateRight(char *word);
+char **CreateMatrix(char *input);
+char *RotateRight(char *word);
+
 
 // Difusion funcs
 
 // Confusion funcs
 // (?)
 
-// Main functions
+
 
 int main(int argc, char *argv[])
 {
-    char *input = "aaaaaaaabbbbbbbbccccccccdddddddd"; //256 bits
-//                    1-------2-------3-------4-------
-    char matrix[4][9];
-    for(int i=0;i<4;i++){ 
-        memcpy(matrix[i], &input[i*8], 8); 
-        matrix[i][8] = '\0';
-    }
+    char *input = "abcdefghabcdefghabcdefghabcdefgh"; //256 bits
+    char **matriz = CreateMatrix(input); 
+
     printf("-> %s = %d\n", input, strlen(input));
-    for(int i=0;i<4;i++){ printf("- %s\n", matrix[i] ); }
+    for(int i=0;i<4;i++){ printf("- %s\n", matriz[i] ); }
+
+    //rotate
+    printf("---\n");
+
+    for(int i=1;i<4;i++){
+        for(int j=0;j<4-i;j++){
+            matriz[i] = RotateRight( matriz[i] );
+        } 
+    }
+    for(int i=0;i<4;i++){ printf("- %s\n", matriz[i] ); }
+
+
 
     return 0;
 }
 
-char* RotateRight(char *word){
-    char result[32] = { word[31] };
-    return strncat(result, word, 31);
+char **CreateMatrix(char *input){
+    char **matriz = malloc(4 * sizeof(char *)); 
+    for (int i=0;i<4;i++) {
+        matriz[i] = malloc(8* sizeof(char));    
+        memcpy( matriz[i], &input[i*8],8 );
+    }
+
+    return matriz;
+}
+
+char *RotateRight(char *word){
+    char *result = malloc( 8*sizeof(char) );
+    result[0] = word[7];
+    return strncat(result, word, 7);
+
 }
 
