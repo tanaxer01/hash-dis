c7e3f6ef7c48386fa72a28c747794d1b653b5295
tanaxer01 <jpbrione@gmail.com>
2021-10-06 20:15:00 -0300

    hachachash

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-# hash-dis
+# Cant Hash Dis
+
+## Funcionamiento
+## Ideas
+### ~~Stealing AES~~
+### Cha Cha
+  - Fast 
+  - "Easy"
+  - Secure
+  - (Update) Es la base para BLAKE
 
 ## Restricciones
-    - [ ] el output debe ser >= 25 chars.
-    - [ ] Debe tener gran difusión.
-    - [ ] % de colisionar debe ser complejo *
-    - [ ] Procesamiento debe ser "rápido"
-## Ideas
-### Stealing AES
-- Para agregar un gran nivel de difusión, ocupar implementación 
-  similar a shiftRows & mixColumn.
-- 4 galoid ops: "Understanding Crypto - Christof Paar"
-                https://en.wikipedia.org/wiki/Finite_field_arithmetic#Rijndael's_(AES)_finite_field
-
-### My Own
-- definir bloques de 256 bits ( 32 bytes ) pq somos AES kool. 
-- tener K iteraciones, donde en cada iteración se agrega otro bloque de texto.
+  - [+] el output debe ser >= 25 chars.
+  - [+] Debe tener gran difusión.
+  - [ ] % de colisionar debe ser complejo *
+  - [+] Procesamiento debe ser "rápido"
+ 
 
 #### Resistencia a Colisiones
 Para saber la probabilidad de una colision, nos podemos apoyar en la "paradoja del cumpleaños".
@@ -22,13 +22,5 @@ Para saber la probabilidad de una colision, nos podemos apoyar en la "paradoja d
 > de que 2 personas tengan el mismo cumpleaños? 
 Donde por ej. con X=50% solamente necesitamos 23 personas. ( Revisar libro )
 
-
-### Pendiente 
-- Cómo hacer menos determinista la custion.
-- Qué hacer para normalizar el input (aka padding)
-- Tmb tiene que ser "reproducible" ( lo que eso signifique xd )
-
-- diff AES128 & AES256 , todo en GF(2^8) ?
-
-
-
+> "The most important consequence of the birthday attack is that the num of messages needed to
+> find a collision is roughly equal to the square root of the num of possible output  values"
--- main.c
+++ main.c
@@ -1,86 +1,119 @@
-#include <string.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-#include "galoid.h"
 
-// Matrix ops
-char **CreateMatrix(char *input);
-char  *RotateRight(char *word);
-
-
-// Difusion funcs
-char **ShiftRows(char **matrix);
-char **MixColumns(char **matrix);
-
-// Confusion funcs
-// (?)
+#define ROTL(a,b) (((a) << (b)) | ((a) >> (32 - (b))))
+#define QR(a, b, c, d) (			\
+	a += b + (c&d),  d ^= a,  d = ROTL(d,16),	\
+	c += d,  b ^= c,  b = ROTL(b,12),	\
+	a += b + (c|d),  d ^= a,  d = ROTL(d, 8),	\
+	c += d,  b ^= c,  b = ROTL(b, 7))
+#define ROUNDS 10
 
 
+char *AddPadding(char const *msg, size_t msg_len, size_t new_len);
+uint32_t *GenState(char *msg);
+uint32_t *UpdateState(uint8_t msg[16]);
+void ChaChaState(uint32_t out[16], uint32_t const in[16]);
 
-int main(int argc, char *argv[])
+// aux
+static uint32_t pack4(const uint8_t *a);
+
+int main(){ 
+
+    char *msg = malloc( 33*sizeof(char) );
+    //msg = "holaholaholaholaholaholaholaholaholaholaholaholaholaholaholaholaholahola"; 
+    msg = "ba";
+    if ( strlen(msg) < 33 )
+       msg = AddPadding(msg, strlen(msg), 32);
+
+    printf("%s \n", msg);
+    uint32_t *state = GenState(msg);
+
+    uint32_t input[16];//512
+    for(int i=0;i<8;++i){ 
+        input[i] = pack4(msg+(i*4));
+        input[i+8] = pack4(msg+(i*4));
+    }
+
+    ChaChaState(input, state);
+    for(int i=0;i<8;i++)
+        printf("%2x",input[i]);
+    printf("\n");
+}
+
+// ** asume que msg_len >= 1
+char *AddPadding(char const *msg, size_t msg_len, size_t new_len)
 {
-    char *input = "abcdabcdabcdabcd"; //128 bits
-    char **matriz = CreateMatrix(input); 
+    /* 
+        Si el mensaje tiene un largo menor a 256 entonces le agregamos padding,
+        compuesto por el mensaje cifrado en cesar con el largo del mensaje.
+    */
 
-    printf("-> %s = %d\n", input, strlen(input));
-    for(int i=0;i<4;i++){ printf("- %s\n", matriz[i] ); }
+    size_t faltan = new_len-msg_len;
+    char *pad = malloc( faltan*sizeof(char) );
+    char *res = malloc( (new_len+1)*sizeof(char) );
 
-    //rotate
-    printf("---\n");
+    for(;strlen(pad)!=faltan;)
+        strncat(pad,msg, (faltan-strlen(pad)<msg_len)?faltan-strlen(pad):msg_len);
 
-    matriz = ShiftRows( matriz );
-    for(int i=0;i<4;i++){ printf("- %s\n", matriz[i] ); }
+    for(int i=0;i<faltan;i++)
+        pad[i] ^= msg_len;
 
-
-
-    return 0;
+    strcpy(res, msg);
+    return strcat(res, pad);
 }
 
-char **CreateMatrix(char *input){
-    /* Parseamos 1 string de 32 bytes en 4 filas de 8 bytes
-       ( Osea una matriz 4x4 donde cada elemento son 2 chars) */
-    char **matriz = malloc(4 * sizeof(char *)); 
-    for (int i=0;i<4;i++) {
-        matriz[i] = malloc(4* sizeof(char));    
-        memcpy( matriz[i], &input[i*4],4 );
+
+uint32_t *GenState(char *msg)
+{
+    uint32_t *state = malloc(16*sizeof(uint32_t));
+    const uint8_t *constant = (uint8_t *)"expand 32-byte k";
+    const uint8_t *mensaje  = (uint8_t *)msg;
+
+    for(int i=0;i<4;++i) // constant
+        state[i] = pack4(constant+(i*4));
+    for(int i=0;i<8;++i) // key
+        state[i+4] = pack4(mensaje+(i*4));
+
+    //counter parte en 0
+    state[12] = 0; 
+    // nonce 
+
+    return state;
+}
+
+void ChaChaState(uint32_t out[16], uint32_t const in[16]){
+    int i;
+    uint32_t x[16];
+
+    for (i = 0; i < 16; ++i)	
+            x[i] = in[i];
+    // 10 loops × 2 rounds/loop = 20 rounds
+    for (i = 0; i < ROUNDS; i += 2) {
+            // Odd round
+            QR(x[0], x[4], x[ 8], x[12]); // column 0
+            QR(x[1], x[5], x[ 9], x[13]); // column 1
+            QR(x[2], x[6], x[10], x[14]); // column 2
+            QR(x[3], x[7], x[11], x[15]); // column 3
+            // Even round
+            QR(x[0], x[5], x[10], x[15]); // diagonal 1 (main diagonal)
+            QR(x[1], x[6], x[11], x[12]); // diagonal 2
+            QR(x[2], x[7], x[ 8], x[13]); // diagonal 3
+            QR(x[3], x[4], x[ 9], x[14]); // diagonal 4
     }
-
-    return matriz;
-}
-
-char *RotateRight(char *word){
-    /* Coloca el utimo char al inicio de la palabra */
-    char *result = malloc( 8*sizeof(char) );
-    result[0] = word[7];
-
-    return strncat(result, word, 7);
-}
-
-char **ShiftRows(char **matriz){
-/* 
-Rota las de la siguiente manera
-  fila 1 -> 0 Shifts
-  fila 2 -> 3 Shifts
-  fila 3 -> 2 Shifts
-  fila 4 -> 1 Shifts 
-*/
-    for(int i=1;i<4;i++){
-        for(int j=0;j<4-i;j++){
-            matriz[i] = RotateRight( matriz[i] );
-        } 
-    }
-
-    return matriz;
-}
-
-char **MixColumns(char **matrix){
-   char mult[4] = "2311"; 
-
-   for(;mult[3] == 2; mult = RotateRight(mult)){
-
-   }
+    for (i = 0; i < 16; ++i)
+            out[i] = x[i] + in[i];
 }
 
 
+static uint32_t pack4(const uint8_t *a){
+    uint32_t res = 0;
+    res |= (uint32_t)a[0];
+    res |= (uint32_t)a[1] << 8 ;
+    res |= (uint32_t)a[2] << 16;
+    res |= (uint32_t)a[3] << 24;
 
-
+    return
+}
